8bda64fdc0c2fa5565b236524124ce01f76452a2
Powar <pramod-balkrushna.powar@capgemini.com>
2022-08-07 18:45:54 +0530

    Style changed for chart bar

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ const DUMMY_EXPENSES = [
   {
     id: "e1",
     title: "Toilet Paper",
-    amount: 94.12,
+    amount: 10.12,
     date: new Date(2020, 7, 14),
   },
-  { 
+  {
     id: "e2",
     title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12) },
+    amount: 20.49,
+    date: new Date(2021, 2, 12),
+  },
   {
     id: "e3",
     title: "Car Insurance",
-    amount: 294.67,
+    amount: 24.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
-    amount: 450,
+    amount: 40,
     date: new Date(2021, 5, 12),
   },
 ];
@@ -31,7 +32,7 @@ function App() {
   const [expenses, setExpense] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    setExpense((prevExpenses)=>{
+    setExpense((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
